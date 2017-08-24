9d266badd04cb479f5daab846ddbd1aad90ba95d
Maximilian Wenger <mwenger@uos.de>
2017-08-24 16:54:26 +0200

    Cleanup thresholding

--- lane_finding/threshold.py
+++ lane_finding/threshold.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-def sob(img, t_min, t_max):
+def sobelx(img, t_min, t_max):
+    """Apply the sobel operator in the x direction and return a binary image."""
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0) # Take the derivative in x
     abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
@@ -10,20 +11,21 @@ def sob(img, t_min, t_max):
     binary[(scaled_sobel >= t_min) & (scaled_sobel <= t_max)] = 255
     return binary
 
-def thresh(img, t_min, t_max):
+def threshold(img, t_min, t_max):
+    """Return the pixels of the binary image between min and max."""
     binary = np.zeros_like(img)
     binary[(img >= t_min) & (img <= t_max)] = 255
     return binary
 
-def conv(img, space, channel):
-    #print(np.max(img))
+def get_channel_in_color_space(img, space, channel):
+    """Convert the given image to the color space specified and return one channel."""
     img_conv = cv2.cvtColor(img, getattr(cv2, "COLOR_RGB2" + space.upper()))
     img_chan = img_conv[:,:,space.find(channel)]
-    normalized = img_chan#np.uint8(255*img_chan/np.max(img_chan))
-    #normalized = np.uint8(255*img_chan/np.max(img_chan))
+    normalized = img_chan #np.uint8(255*img_chan/np.max(img_chan)) # (necessary for jupyter)
     return normalized
 
-def comb(images, limit=15*1e6):
+def combine_sparse(images, limit=15*1e6):
+    """Combines the images in the list if the are sparse enough."""
     combined = np.zeros_like(images[0])
     for img in images:
         if np.sum(img) < limit:
@@ -34,17 +36,20 @@ def threshold_basic(img):
     """Threshold a grayscale image and return the binary image."""
     assert len(img.shape) == 3, "only color images are supported"
 
-    sobel = sob(img, 20, 200)
-    #sobel = sob(img, 20, 100)
+    # create a binary image using the sobel operator
+    sobel = sobelx(img, 20, 200)
 
-    one = thresh(conv(img, 'hsv', 'v'), 210, 255)
-    two = thresh(conv(img, 'hls', 's'), 110, 255)
-    three = thresh(conv(img, 'hls', 'l'), 140, 250)
-    color = comb([one, two, three])
+    # create binary images of different tresholded color channels
+    hsv_v = threshold(get_channel_in_color_space(img, 'hsv', 'v'), 210, 255)
+    hls_s = threshold(get_channel_in_color_space(img, 'hls', 's'), 110, 255)
+    hls_l = threshold(get_channel_in_color_space(img, 'hls', 'l'), 140, 250)
 
-    combined_binary = comb([color, sobel], limit=20*1e6)
-    if np.sum(combined_binary) == 0:
-        combined_binary = sobel
+    # combine the thresholded images
+    color = combine_sparse([hsv_v, hls_s, hls_l])
+
+    # combine the thresholded and sobel images
+    combined_binary = combine_sparse([color, sobel], limit=20*1e6)
+
     assert np.sum(combined_binary) > 0
     return combined_binary
 
